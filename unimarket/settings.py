fd5ba0ed6d27af8f860395ecd44ec6a732dd107c
jonathan <kalalajonathan297@gmail.com>
2026-06-20 22:40:32 +0100

    Correction staticfiles

--- unimarket/settings.py
+++ unimarket/settings.py
@@ -177,7 +177,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Activer la compression et la mise en cache (optionnel mais recommandé)
 STORAGES = {
