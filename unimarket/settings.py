f12b649efdadda28768a738c6a49e1a107b108bc
jonathan <kalalajonathan297@gmail.com>
2026-06-20 22:04:29 +0100

    Correction settings pour environ

--- unimarket/settings.py
+++ unimarket/settings.py
@@ -13,7 +13,6 @@ import os
 import environ 
 from pathlib import Path
 import dj_database_url
-from gunicorn.config import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,7 +82,7 @@ WSGI_APPLICATION = 'unimarket.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(Env('DATABASE_URL'))
+    'default': dj_database_url.parse(env('DATABASE_URL'))
     }
 
 # ============================================
